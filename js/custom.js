b2604f038156484432ab7b055dbf0d39c357d1be
Ashik <ashikariyan503@gmail.com>
2022-09-19 21:19:50 +0600

    some changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,17 @@
-$('.nav').on('click', function(){
-    $('.nav').removeClass('active');
-    $(this).addClass('active');
-});
+
+
+// $('.nav').on('click', function(){
+//     $('.nav').removeClass('active');
+//     $(this).addClass('active');
+    
+// });
+// console.log('.nav');
+// $(document).ready(function() {
+//     $(".nav").click(function () {
+//         if(!$(this).hasClass('active'))
+//         {
+//             $(".nav.active").removeClass("active");
+//             $(this).addClass("active");        
+//         }
+//     });
+// });
